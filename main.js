e2c18714525a8832428d38b19322973d98cbe565
yfuku <122361817+y-fukuhara97@users.noreply.github.com>
2023-03-14 21:03:37 +0900

    時間結果の表示を実装

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const time = document.getElementById('time');
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
 const resetButton = document.getElementById('reset');
+const result = document.getElementById('result');
+
 
 // 開始時間
 let startTime;
@@ -10,12 +12,25 @@ let stopTime = 0;
 // タイムアウトID
 let timeoutID;
 
+// 現在時間の情報
+let currentTime;
+
 // 時間を表示する関数
 function displayTime() {
-  const currentTime = new Date(Date.now() - startTime + stopTime);
+  currentTime = new Date(Date.now() - startTime + stopTime);
   const s = String(currentTime.getSeconds()).padStart(2, '0');
   const ms = String(currentTime.getMilliseconds()).padStart(3, '0');
-
+  
+  // console.log(currentTime.getSeconds());
+  // console.log(currentTime.getMilliseconds());
+  // console.log(currentTime.getSeconds() + (currentTime.getMilliseconds() / 1000));
+  
+  
+  // console.log(startTime);
+  // console.log(stopTime);
+  // console.log(currentTime);
+  
+  
   time.textContent = `${s}.${ms}`;
   timeoutID = setTimeout(displayTime, 10);
 }
@@ -27,7 +42,7 @@ startButton.addEventListener('click', () => {
   resetButton.disabled = true;
   startTime = Date.now();
   displayTime();
-  time.classList.add('hide');
+  // time.classList.add('hide');
 
 
 });
@@ -39,8 +54,14 @@ stopButton.addEventListener('click', function() {
   resetButton.disabled = false;
   clearTimeout(timeoutID);
   stopTime += (Date.now() - startTime);
-  time.classList.remove('hide');
-  console.log(stopTime);
+  // time.classList.remove('hide');
+  // console.log(stopTime);
+  console.log(currentTime);
+  console.log(currentTime.getSeconds() + (currentTime.getMilliseconds() / 1000));
+  console.log(2.5 - (currentTime.getSeconds() + (currentTime.getMilliseconds() / 1000)));
+  
+  const resultTime = 2.5 - (currentTime.getSeconds() + (currentTime.getMilliseconds() / 1000));
+  result.textContent = resultTime;
 });
 
 // リセットボタンがクリックされたら時間を0に戻す
