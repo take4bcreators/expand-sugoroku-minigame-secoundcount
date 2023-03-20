6bc4f940d91a989ea85764affe6a1c907e7ccbf1
yfuku <122361817+y-fukuhara97@users.noreply.github.com>
2023-03-20 21:50:19 +0900

    目標時間設定を実装

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const time = document.getElementById('time');
-const startButton = document.getElementById('start');
-const stopButton = document.getElementById('stop');
-const resetButton = document.getElementById('reset');
-const result = document.getElementById('result');
+'use strict';
+
+// HTMLの要素を取得して変数に入れる
+const time = document.getElementById('time'); // 時間表示の要素
+const startButton = document.getElementById('start'); // スタートボタンの要素
+const stopButton = document.getElementById('stop'); // ストップボタンの要素
+const resetButton = document.getElementById('reset'); // リセットボタンの要素
+const setButton = document.getElementById('set'); // セットボタンの要素
+const result = document.getElementById('result'); // 結果表示の要素
+const targetElem = document.getElementById('target'); // 目標タイム表示の要素
+
 
 
 // 開始時間
@@ -14,6 +20,33 @@ let timeoutID;
 
 // 現在時間の情報
 let currentTime;
+
+
+let targetTime = 1;
+
+const minTime = 5;    // 出る数字の最小値
+const maxTime = 15;   // 出る数字の最大値
+const digitNum = 10;
+
+// 0 〜 1未満のランダムな数字を生成
+const randomNum = Math.random();
+const randomNum2 = (Math.floor(randomNum * (maxTime + 1 - minTime) * 10) / 10) + minTime;
+targetTime = randomNum2;
+targetElem.textContent = targetTime;
+
+
+setButton.addEventListener('click', () => {
+  const randomNum = Math.random();
+  const randomNum2 = (Math.floor(randomNum * (maxTime + 1 - minTime) * 10) / 10) + minTime;
+  targetTime = randomNum2;
+  targetElem.textContent = targetTime;
+});
+
+
+
+
+
+
 
 // 時間を表示する関数
 function displayTime() {
@@ -58,10 +91,14 @@ stopButton.addEventListener('click', function() {
   // console.log(stopTime);
   console.log(currentTime);
   console.log(currentTime.getSeconds() + (currentTime.getMilliseconds() / 1000));
-  console.log(2.5 - (currentTime.getSeconds() + (currentTime.getMilliseconds() / 1000)));
+  console.log(targetTime - (currentTime.getSeconds() + (currentTime.getMilliseconds() / 1000)));
   
-  const resultTime = 2.5 - (currentTime.getSeconds() + (currentTime.getMilliseconds() / 1000));
-  result.textContent = resultTime;
+  // 目標タイムとの差の時間
+  const resultTime = targetTime - (currentTime.getSeconds() + (currentTime.getMilliseconds() / 1000));
+  // 桁を絞った数字を変数に入れる（resultTimeFloor）
+  const resultTimeFloor = Math.floor(resultTime * 1000) / 1000;
+  // プラスマイナスを入れ替えて表示する
+  result.textContent = resultTimeFloor * -1;
 });
 
 // リセットボタンがクリックされたら時間を0に戻す
@@ -72,3 +109,11 @@ resetButton.addEventListener('click', function() {
   time.textContent = '00.000';
   stopTime = 0;
 });
+
+
+const num = 5.339;              // 5.339 →  5.33
+const num2 = num * 100;         // 533.9
+const num3 = Math.floor(num2);  // 533
+const num4 = num3 / 100;        // 5.33
+
+console.log(Math.floor(5.339 * 100) / 100);
